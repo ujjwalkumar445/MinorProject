37b0caeed4a7909b37f3b19e493d571efe32f359
ujjwal@ujjwal <ujjwal.kumar@tothenew.com>
2020-04-10 18:59:27 +0530

    Login & Signup Screen

---
+++ app/src/main/java/com/example/minorproject/SignUpScreen.kt
@@ -0,0 +1,40 @@
+package com.example.minorproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignUpScreen : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.registerButton).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.registerButton -> navController!!.navigate(R.id.action_signUpScreen_to_loginScreen)
+        }
+
+    }
+
+}
